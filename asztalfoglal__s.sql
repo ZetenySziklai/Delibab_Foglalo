ad4ddef8712fe52c1562a39099ad1b3303850ca6
Sziklai Zétény <sziklai.zeteny@ckik.loc>
2025-10-03 08:33:47 +0200

    osszekotottadatbazis'

--- asztalfoglal__s.sql
+++ asztalfoglal__s.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2025. Okt 03. 08:13
+-- Létrehozás ideje: 2025. Okt 03. 08:32
 -- Kiszolgáló verziója: 10.4.32-MariaDB
 -- PHP verzió: 8.2.12
 
@@ -33,7 +33,10 @@ CREATE TABLE `foglaló` (
   `telefonszám` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `megjegyzés` varchar(255) NOT NULL,
-  `foglalo_id` int(255) NOT NULL
+  `foglalo_id` int(255) NOT NULL,
+  `idopont_id` int(11) NOT NULL,
+  `vendeg_id` int(11) NOT NULL,
+  `etkezes_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -80,7 +83,10 @@ CREATE TABLE `étkezés_típusa` (
 -- A tábla indexei `foglaló`
 --
 ALTER TABLE `foglaló`
-  ADD PRIMARY KEY (`foglalo_id`);
+  ADD PRIMARY KEY (`foglalo_id`),
+  ADD KEY `idopont_id` (`idopont_id`),
+  ADD KEY `vendeg_id` (`vendeg_id`),
+  ADD KEY `etkezes_id` (`etkezes_id`);
 
 --
 -- A tábla indexei `időpont`
@@ -93,6 +99,24 @@ ALTER TABLE `időpont`
 --
 ALTER TABLE `vendégek_száma`
   ADD PRIMARY KEY (`vendeg_id`);
+
+--
+-- A tábla indexei `étkezés_típusa`
+--
+ALTER TABLE `étkezés_típusa`
+  ADD PRIMARY KEY (`etkezes_id`);
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `foglaló`
+--
+ALTER TABLE `foglaló`
+  ADD CONSTRAINT `foglaló_ibfk_1` FOREIGN KEY (`idopont_id`) REFERENCES `időpont` (`időpont_id`),
+  ADD CONSTRAINT `foglaló_ibfk_2` FOREIGN KEY (`vendeg_id`) REFERENCES `vendégek_száma` (`vendeg_id`),
+  ADD CONSTRAINT `foglaló_ibfk_3` FOREIGN KEY (`etkezes_id`) REFERENCES `étkezés_típusa` (`etkezes_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
