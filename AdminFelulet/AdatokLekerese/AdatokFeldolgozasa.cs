f33d7b870fe4905317745da8e7ee0c20207036dc
Sziklai Zétény <sziklai.zeteny@ckik.hu>
2025-11-19 20:10:57 +0100

    http_client_beagyazas(proba)

--- AdminFelulet/AdatokLekerese/AdatokFeldolgozasa.cs
+++ AdminFelulet/AdatokLekerese/AdatokFeldolgozasa.cs
@@ -3,20 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
 namespace AdatokElerese
 {
     public class AdatokFeldolgozasa
     {
-        public string ido;
-        public List<bool> asztal;
+        [JsonPropertyName("ido")]
+        public string ido { get; set; }
 
+        [JsonPropertyName("asztal")]
+        public List<bool> asztal { get; set; }
+
+        // Default constructor for JSON deserialization
+        public AdatokFeldolgozasa()
+        {
+            asztal = new List<bool>();
+        }
+
+        // Constructor for CSV parsing (if you still need it)
         public AdatokFeldolgozasa(string sor)
         {
-            
+            asztal = new List<bool>(); // Initialize the list
             string[] adatok = sor.Split(';');
             ido = adatok[0];
-            for(int i = 1; i < adatok.Length; i++)
+            for (int i = 1; i < adatok.Length; i++)
             {
                 asztal.Add(Convert.ToBoolean(adatok[i]));
             }
