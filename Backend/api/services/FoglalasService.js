ba0906c4d967f307c8d92fbfc2a3800f020b3d5d
farkasmate69 <farkas.mate@ckik.hu>
2026-03-12 13:44:38 +0100

    createUser javitas

--- Backend/api/services/FoglalasService.js
+++ Backend/api/services/FoglalasService.js
@@ -12,9 +12,6 @@ class FoglalasService {
     }
 
 
-
-
-    
     async createFoglalas(data, options = {}) {
         if (!data.user_id || !data.asztal_id || !data.foglalas_datum) {
             throw new BadRequestError("Minden kötelező mezőt ki kell tölteni");
@@ -41,8 +38,42 @@ class FoglalasService {
 
         if (vanDuplikatum) throw new BadRequestError("Az asztal már foglalt ezen az időponton");
 
+        // Validáció után felülírjuk a mostani időpontra
+        data.foglalas_datum = new Date();
+
         return await this.foglalasRepository.createFoglalas(data, options);
     }
+
+
+
+    // async createFoglalas(data, options = {}) {
+    //     if (!data.user_id || !data.asztal_id || !data.foglalas_datum) {
+    //         throw new BadRequestError("Minden kötelező mezőt ki kell tölteni");
+    //     }
+
+    //     const user = await this.userRepository.getUserById(data.user_id, options);
+    //     if (!user) throw new NotFoundError("A megadott felhasználó nem létezik");
+
+    //     const asztal = await this.asztalRepository.getAsztalById(data.asztal_id, options);
+    //     if (!asztal) throw new NotFoundError("A megadott asztal nem létezik");
+
+    //     const foglalasDatum = new Date(data.foglalas_datum);
+    //     if (isNaN(foglalasDatum.getTime()) || foglalasDatum < new Date()) {
+    //         throw new BadRequestError("Érvénytelen vagy múltbeli dátum");
+    //     }
+
+    //     const datumResz = data.foglalas_datum.split(' ')[0];
+    //     const foglaltIdopontok = await this.foglalasRepository.getFoglaltIdopontok(datumResz, data.asztal_id, options);
+
+    //     const vanDuplikatum = foglaltIdopontok.some(foglalas => {
+    //         const kulonbseg = Math.abs(new Date(foglalas.foglalas_datum).getTime() - foglalasDatum.getTime());
+    //         return kulonbseg < 3600000;
+    //     });
+
+    //     if (vanDuplikatum) throw new BadRequestError("Az asztal már foglalt ezen az időponton");
+
+    //     return await this.foglalasRepository.createFoglalas(data, options);
+    // }
 
 
 
