c5753e788c2c0e2926a68891861ad1ca7b773029
deakzsombor1 <deak.zsombor@ckik.hu>
2026-03-30 09:27:15 +0200

    doksi eredetisegi nyilatkozat feltoltes

--- AdminWPF/AdminWPF.Tests/obj/Debug/net8.0/AdminWPF.Tests.AssemblyInfo.cs
+++ AdminWPF/AdminWPF.Tests/obj/Debug/net8.0/AdminWPF.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdminWPF.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+386475445be00420645d66c964e577cc6339b0c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+935293b0f107e54365f26e9ddbd17d5259662875")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdminWPF.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdminWPF.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- AdminWPF/AdminWPF/obj/Debug/net8.0-windows/App.g.cs
+++ AdminWPF/AdminWPF/obj/Debug/net8.0-windows/App.g.cs
@@ -46,7 +46,7 @@ namespace AdminWPF {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.7.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -72,7 +72,7 @@ namespace AdminWPF {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.7.0")]
         public static void Main() {
             AdminWPF.App app = new AdminWPF.App();
             app.InitializeComponent();
